c0967c397bce0b2f9602fe96e10c52ca8801f097
Marishel Lochan <marishel.lochan@my.uwi.edu>
2025-11-29 00:17:47 -0400

    Refactored Integration test Part 1 & Controllers
    auth controller
    - authenticate added
    - jwt_authenticate added
    - is_staff, is_employer, is_student added
    - logout added
    
    user controller:
    - refactored create_user
    - refactored get_all_users_json
    
    position:
    - view_positions_by_employer added
    
    test_app.py
    23 tests so far

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,29 +1,36 @@
 from App.models import User, Student, Employer, Staff
 from App.database import db
 
-def create_user(username, password, user_type):
-    try:
-        newuser = User(username=username, password=password, role=user_type)
-        db.session.add(newuser)
-        db.session.flush() 
+# def create_user(username, password, user_type):
+#     try:
+#         newuser = User(username=username, password=password, role=user_type)
+#         db.session.add(newuser)
+#         db.session.flush() 
         
-        if user_type == "student":
-            student = Student(username=username, user_id=newuser.id)
-            db.session.add(student)
-        elif user_type == "employer":
-            employer = Employer(username=username, user_id=newuser.id)
-            db.session.add(employer)
-        elif user_type == "staff":
-            staff = Staff(username=username, user_id=newuser.id)
-            db.session.add(staff)
-        else:
-            return False
+#         if user_type == "student":
+#             student = Student(username=username, user_id=newuser.id)
+#             db.session.add(student)
+#         elif user_type == "employer":
+#             employer = Employer(username=username, user_id=newuser.id)
+#             db.session.add(employer)
+#         elif user_type == "staff":
+#             staff = Staff(username=username, user_id=newuser.id)
+#             db.session.add(staff)
+#         else:
+#             return False
         
-        db.session.commit()
-        return True
-    except Exception as e:
-        db.session.rollback()
-        return False
+#         db.session.commit()
+#         return newuser
+#     except Exception as e:
+#         db.session.rollback()
+#         return False
+
+def create_user(username, password, email): #Marishel : added this create user instead 
+    newuser = User(username=username, password=password, email=email)
+    db.session.add(newuser)
+    db.session.commit()
+    return newuser
+
 
 def get_user_by_username(username):
     result = db.session.execute(db.select(User).filter_by(username=username))
@@ -35,20 +42,12 @@ def get_user(id):
 def get_all_users():
     return db.session.scalars(db.select(User)).all()
 
-def get_all_users_json():
+def get_all_users_json(): #Marishel : refactored this 
     users = get_all_users()
     if not users:
         return []
-    
-    return [
-        {
-            "id": user.id,
-            "username": user.username,
-            "email": getattr(user, "email", None),
-            "user_type": getattr(user, "role", None)
-        }
-        for user in users
-    ]
+    users = [user.get_json() for user in users]
+    return users
 
 def update_user(id, username):
     user = get_user(id)
