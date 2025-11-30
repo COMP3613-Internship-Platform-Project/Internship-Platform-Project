94d4429a7e6e428ba8b000e4224600ae327f1bed
Jamal Mohammed <jamalm2427@gmail.com>
2025-11-30 16:30:14 -0400

    cleaning up .gitignore

--- App/views/shortlist.py
+++ App/views/shortlist.py
@@ -5,24 +5,7 @@ from flask_jwt_extended import jwt_required, current_user
 from App.controllers.application import add_application_to_shortlist
 
 
-shortlist_views = Blueprint('shortlist_views', __name__)
-
-
-@shortlist_views.route('/api/shortlist', methods = ['POST'])
-@jwt_required()
-def add_student_shortlist():
-     if current_user.role != 'staff':
-        return jsonify({"message": "Unauthorized user"}), 403
-    
-     data = request.json
-     request_result = add_application_to_shortlist(student_id=data['student_id'], position_id=data['position_id'], staff_id=current_user.id)
-     
-     if request_result:
-         return jsonify(request_result.toJSON()), 200
-     else:
-         return jsonify({"error": "Failed to add to shortlist"}), 401
-     
-     
+shortlist_views = Blueprint('shortlist_views', __name__, template_folder='../templates')
 
 @shortlist_views.route('/api/shortlist/student/<int:student_id>', methods = ['GET'])
 @jwt_required()
@@ -35,24 +18,6 @@ def get_student_shortlist(student_id):
     shortlists = get_shortlist_by_student(student_id)
     
     return jsonify([s.toJSON() for s in shortlists]), 200
-    
-
-
-# @shortlist_views.route('/api/shortlist',methods = ['PUT'] ) 
-# @jwt_required()
-# def shortlist_decide():
-#     if current_user.role != 'employer':
-#         return jsonify({"message": "Unauthorized user"}), 403
-    
-    
-#     data = request.json
-#     request_result = decide_shortlist(data['student_id'], data['position_id'], data['decision'])
-   
-#     if request_result:
-#         return jsonify(request_result.toJSON()), 200
-#     else:
-#      return jsonify({"error": "Failed to update shortlist"}), 400
-    
 
 @shortlist_views.route('/api/shortlist/position/<int:position_id>', methods=['GET'])
 @jwt_required()
